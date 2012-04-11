00388f43b5d7e0609ed71fd483cdf1b6109ad4aa
rahul0996 <rgarg@thoughtworks.com>
2012-04-11 16:19:22 +0530

    Ashihsh, Rahul | #2 | added submit url while submitting caseXml

--- case-integration/commcare-case-gateway/src/main/java/org/motechproject/casexml/gateway/CommcareCaseGateway.java
+++ case-integration/commcare-case-gateway/src/main/java/org/motechproject/casexml/gateway/CommcareCaseGateway.java
@@ -2,32 +2,26 @@ package org.motechproject.casexml.gateway;
 
 import org.motechproject.casexml.domain.CaseTask;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Properties;
 
 @Component
 public class CommcareCaseGateway{
     private CaseTaskXmlConverter caseTaskXmlConverter;
-    private Properties commcareProperties;
     private RestTemplate restTemplate;
 
     @Autowired
-    public CommcareCaseGateway(CaseTaskXmlConverter caseTaskXmlConverter, @Qualifier("commcareProperties") Properties properties) {
-        this(caseTaskXmlConverter, properties, new RestTemplate());
+    public CommcareCaseGateway(CaseTaskXmlConverter caseTaskXmlConverter) {
+        this(caseTaskXmlConverter, new RestTemplate());
     }
 
-    public CommcareCaseGateway(CaseTaskXmlConverter caseTaskXmlConverter, Properties properties, RestTemplate restTemplate) {
+    public CommcareCaseGateway(CaseTaskXmlConverter caseTaskXmlConverter, RestTemplate restTemplate) {
         this.caseTaskXmlConverter = caseTaskXmlConverter;
-        commcareProperties = properties;
         this.restTemplate = restTemplate;
     }
 
-    public void submitCase(CaseTask task){
+    public void submitCase(String commcareUrl, CaseTask task){
         String request = caseTaskXmlConverter.convertToCaseXmlWithEnvelope(task);
-        String url = commcareProperties.getProperty("commcare.hq.url");
-        restTemplate.postForLocation(url, request);
+        restTemplate.postForLocation(commcareUrl, request);
     }
 }
--- case-integration/commcare-case-gateway/src/test/java/org/motechproject/casexml/gateway/CommcareCaseGatewayTest.java
+++ case-integration/commcare-case-gateway/src/test/java/org/motechproject/casexml/gateway/CommcareCaseGatewayTest.java
@@ -1,14 +1,12 @@
-package org.motechproject.commcare.gateway;
+package org.motechproject.casexml.gateway;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.motechproject.commcare.domain.CaseTask;
+import org.motechproject.casexml.domain.CaseTask;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Properties;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,23 +18,20 @@ public class CommcareCaseGatewayTest  {
     private CaseTaskXmlConverter caseTaskXmlConverter;
 
     @Mock
-    protected Properties commcareProperties;
-    @Mock
     protected RestTemplate restTemplate;
 
     private CommcareCaseGateway commcareCaseGateway;
 
     @Before
     public void before(){
-        commcareCaseGateway = new CommcareCaseGateway(caseTaskXmlConverter,commcareProperties, restTemplate );
+        commcareCaseGateway = new CommcareCaseGateway(caseTaskXmlConverter, restTemplate );
     }
 
     @Test
     public void shouldFormXmlRequestAndPostItToCommCare(){
         CaseTask task = new CaseTask();
-        when(commcareProperties.getProperty("commcare.hq.url")).thenReturn("someUrl");
         when(caseTaskXmlConverter.convertToCaseXmlWithEnvelope(task)).thenReturn("request");
-        commcareCaseGateway.submitCase(task);
+        commcareCaseGateway.submitCase("someUrl", task);
         verify(caseTaskXmlConverter).convertToCaseXmlWithEnvelope(task);
         verify(restTemplate).postForLocation("someUrl", "request");
     }
