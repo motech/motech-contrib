b097a010e6d025aa37e808cb3bbdb2d91e2dd866
Ashish Kumar Mishra <ashishkm@thoughtworks.com>
2012-04-26 18:24:08 +0530

    Rahul, Ashish |  Fixing test case for xml comparision

--- case-integration/case-service/src/test/java/org/motechproject/casexml/service/response/ResponseMessageBuilderTest.java
+++ case-integration/case-service/src/test/java/org/motechproject/casexml/service/response/ResponseMessageBuilderTest.java
@@ -1,14 +1,27 @@
 package org.motechproject.casexml.service.response;
 
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import org.apache.velocity.app.VelocityEngine;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.motechproject.casexml.service.exception.CaseException;
+import org.motechproject.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,7 +60,7 @@ public class ResponseMessageBuilderTest {
                 "        </error>" + newline +
                 "    </errors>" + newline +
                 "</OpenRosaResponse>";
-        assertEquals(expectedResponse, responseMessage);
+        compareXmlString(expectedResponse, responseMessage);
     }
 
     @Test
@@ -61,7 +74,7 @@ public class ResponseMessageBuilderTest {
         String expectedResponse = "<OpenRosaResponse xmlns=\"http://openrosa.org/http/response\">" + newline +
                 "    <message nature=\"submit_error\">Validation failed</message>" + newline +
                 "</OpenRosaResponse>";
-        assertEquals(expectedResponse,responseMessage);
+        compareXmlString(expectedResponse,responseMessage);
     }
 
     @Test
@@ -72,7 +85,7 @@ public class ResponseMessageBuilderTest {
         String expectedResponse = "<OpenRosaResponse xmlns=\"http://openrosa.org/http/response\">" + newline +
                 "    <message nature=\"submit_success\">Request successfully processed.</message>" + newline +
                 "</OpenRosaResponse>";
-        assertEquals(expectedResponse, successResponse);
+        compareXmlString(expectedResponse, successResponse);
     }
 
     @Test
@@ -83,6 +96,51 @@ public class ResponseMessageBuilderTest {
         String expectedResponse = "<OpenRosaResponse xmlns=\"http://openrosa.org/http/response\">" + newline +
                 "    <message nature=\"submit_error\">An unexpected exception occurred while processing. Please verify the message and try again.</message>" + newline +
                 "</OpenRosaResponse>";
-        assertEquals(expectedResponse, errorResponse);
+        compareXmlString(expectedResponse, errorResponse);
+    }
+
+    private void compareXmlString(String expected, String actual) {
+        expected = toString(documentFrom(expected));
+        actual = toString(documentFrom(actual));
+        assertEquals(expected, actual);
+    }
+
+    private Document documentFrom(String xmlDocument) {
+        if(StringUtil.isNullOrEmpty(xmlDocument)) {
+            throw new IllegalArgumentException();
+        }
+
+        DOMParser parser = new DOMParser();
+
+        InputSource inputSource = new InputSource();
+        inputSource.setCharacterStream(new StringReader(xmlDocument));
+
+        try {
+            parser.parse(inputSource);
+            return parser.getDocument();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        catch (SAXException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private String toString(Document doc) {
+
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();;
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            StringWriter stringWriter = new StringWriter();
+            StreamResult result = new StreamResult(stringWriter);
+            DOMSource source = new DOMSource(doc);
+            transformer.transform(source, result);
+            String xmlString = stringWriter.toString();
+            return xmlString;
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
