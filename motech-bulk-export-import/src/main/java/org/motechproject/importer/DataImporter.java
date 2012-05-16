9b0320cc7a5185758e526ea0925e73a9b43f8910
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-05-16 17:21:22 +0530

    Balaji/Geetha | Added  CSV Importer  classes to  motech-export-import-module

---
+++ motech-bulk-export-import/src/main/java/org/motechproject/importer/DataImporter.java
@@ -0,0 +1,27 @@
+package org.motechproject.importer;
+
+import java.util.List;
+
+public abstract class DataImporter<T> {
+
+    public abstract List<T> parse(String filePath, Class<T> clazz);
+
+    /* override this  for  validation specific to Implementation*/
+    public boolean validate(List<T> entities) {
+        return true;
+    }
+
+    public abstract List<T> transform(List<T> entities);
+
+    public abstract void post(List<T> entities);
+
+    public void process(String filePath, Class<T> clazz) {
+        List<T> valuesFromFile = parse(filePath, clazz);
+        if (validate(valuesFromFile)) {
+            List<T> entities = transform(valuesFromFile);
+            post(entities);
+        }
+    }
+
+
+}
