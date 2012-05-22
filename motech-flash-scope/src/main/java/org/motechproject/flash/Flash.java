8d32e990c012e93cc2cca742b572ac8fbf4db6b5
Balaji Narain <narainbalaji@gmail.com>
2012-05-22 14:01:41 +0530

    BalajiN | #148(WHP) | restricting the access ofsome methods

--- motech-flash-scope/src/main/java/org/motechproject/flash/Flash.java
+++ motech-flash-scope/src/main/java/org/motechproject/flash/Flash.java
@@ -7,26 +7,26 @@ public class Flash {
     public static final String FLASH_IN_PREFIX = "flash.in.";
     public static final String FLASH_OUT_PREFIX = "flash.out.";
 
-    public static boolean shouldBeConsumed(String attributeName) {
-        return attributeName.startsWith(FLASH_OUT_PREFIX);
-    }
-
-    public static boolean shouldBeDestroyed(String attributeName) {
-        return attributeName.startsWith(FLASH_IN_PREFIX);
-    }
-
     private static String simpleAttributeName(String attributeName) {
         return attributeName.substring(FLASH_OUT_PREFIX.length());
     }
 
-    public static boolean has(String attributeName, HttpServletRequest request) {
-        return request.getAttribute(FLASH_IN_PREFIX + attributeName) != null;
+    static boolean shouldBeConsumed(String attributeName) {
+        return attributeName.startsWith(FLASH_OUT_PREFIX);
     }
 
-    public static void transfer(String attributeName, String attributeValue, HttpServletRequest request) {
+    static boolean shouldBeDestroyed(String attributeName) {
+        return attributeName.startsWith(FLASH_IN_PREFIX);
+    }
+
+    static void transfer(String attributeName, String attributeValue, HttpServletRequest request) {
         if (shouldBeConsumed(attributeName) && attributeValue != null) {
             request.setAttribute(FLASH_IN_PREFIX + simpleAttributeName(attributeName), attributeValue);
         }
+    }
+
+    public static boolean has(String attributeName, HttpServletRequest request) {
+        return request.getAttribute(FLASH_IN_PREFIX + attributeName) != null;
     }
 
     public static String in(String attributeName, HttpServletRequest request) {
