8b740bb96d9dbc6e2663fafa26225567edecba9b
shruthi <shruthidipali@gmail.com>
2012-05-24 12:55:30 +0530

    BalajiN, Shruthi | #134(WHP) | Adding more constructors for OpenRosaRegistrationValidationException

--- case-integration/openrosa-registration-service/src/main/java/org/motechproject/provider/registration/exception/OpenRosaRegistrationValidationException.java
+++ case-integration/openrosa-registration-service/src/main/java/org/motechproject/provider/registration/exception/OpenRosaRegistrationValidationException.java
@@ -3,18 +3,29 @@ package org.motechproject.provider.registration.exception;
 import org.motechproject.casexml.service.exception.CaseException;
 import org.springframework.http.HttpStatus;
 
+import java.util.Map;
+
 public class OpenRosaRegistrationValidationException extends CaseException {
 
 
     public OpenRosaRegistrationValidationException(Exception cause, HttpStatus statusCode) {
-        super(cause,statusCode);
+        super(cause, statusCode);
     }
 
     public OpenRosaRegistrationValidationException(Exception cause, String message, HttpStatus statusCode) {
-        super(cause,message,statusCode);
+        super(cause, message, statusCode);
     }
 
     public OpenRosaRegistrationValidationException(String message, HttpStatus statusCode) {
-        super(message,statusCode);
+        super(message, statusCode);
     }
+
+    public OpenRosaRegistrationValidationException(Exception cause, String message, HttpStatus httpStatusCode, Map<String, String> errorMessages) {
+        super(cause, message, httpStatusCode, errorMessages);
+    }
+
+    public OpenRosaRegistrationValidationException(String message, HttpStatus httpStatusCode, Map<String, String> errorMessages) {
+        super(message, httpStatusCode, errorMessages);
+    }
+
 }
