5848755fdb978dbe6c546939ecde6654924b4838
Balaji Narain <narainbalaji@gmail.com>
2012-05-25 10:52:02 +0530

    Shruthi, BalajiN | #0 (WHP) | support validation of null or blank strings

--- motech-validation/src/main/java/org/motechproject/validation/validator/NotNullOrEmptyValidator.java
+++ motech-validation/src/main/java/org/motechproject/validation/validator/NotNullOrEmptyValidator.java
@@ -1,6 +1,6 @@
 package org.motechproject.validation.validator;
 
-import org.motechproject.validation.constraints.NotNull;
+import org.motechproject.validation.constraints.NotNullOrEmpty;
 import org.motechproject.validation.validator.root.PropertyValidator;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -8,13 +8,15 @@ import org.springframework.validation.Errors;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
+import static org.apache.commons.lang.StringUtils.isBlank;
+
 @Component
-public class NotNullValidator extends PropertyValidator {
+public class NotNullOrEmptyValidator extends PropertyValidator {
 
     @Override
     public void validateField(Object target, Field field, String scope, Errors errors) {
-        if (field.isAnnotationPresent(NotNull.class)) {
-            String[] specifiedScopes = field.getAnnotation(NotNull.class).scope();
+        if (field.isAnnotationPresent(NotNullOrEmpty.class)) {
+            String[] specifiedScopes = field.getAnnotation(NotNullOrEmpty.class).scope();
             if (scope == null || specifiedScopes.length == 0 || Arrays.asList(specifiedScopes).contains(scope)) {
                 if (isInValid(target, field)) {
                     errors.rejectValue(field.getName(), "invalid-data", "value should not be null");
@@ -26,7 +28,8 @@ public class NotNullValidator extends PropertyValidator {
     private boolean isInValid(Object target, Field field) {
         field.setAccessible(true);
         try {
-            return field.get(target) == null;
+            Object value = field.get(target);
+            return value == null || isBlank(value.toString());
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
--- motech-validation/src/test/java/org/motechproject/validation/validator/NotNullOrEmptyValidatorTest.java
+++ motech-validation/src/test/java/org/motechproject/validation/validator/NotNullOrEmptyValidatorTest.java
@@ -3,7 +3,7 @@ package org.motechproject.validation.validator;
 import lombok.Data;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.motechproject.validation.constraints.NotNull;
+import org.motechproject.validation.constraints.NotNullOrEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,16 +13,16 @@ import static junit.framework.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:/applicationValidationContext.xml")
-public class NotNullValidatorTest {
+public class NotNullOrEmptyValidatorTest {
 
     @Autowired
-    private NotNullValidator notNullValidator;
+    private NotNullOrEmptyValidator notNullOrEmptyValidator;
 
     @Test
     public void shouldNotReportErrorWhenFieldIsNotNull_AndItsScopeIsNotSpecified() throws NoSuchFieldException {
         ClassWithValidations target = new ClassWithValidations("notNull", "someValue", "someValue");
         BeanPropertyBindingResult errors = new BeanPropertyBindingResult(target, "classWithValidations");
-        notNullValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutScope"), "matchingScope", errors);
+        notNullOrEmptyValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutScope"), "matchingScope", errors);
         assertEquals(0, errors.getFieldErrors().size());
     }
 
@@ -30,7 +30,7 @@ public class NotNullValidatorTest {
     public void shouldReportErrorWhenFieldIsNull_AndItsScopeIsNotSpecified() throws NoSuchFieldException {
         ClassWithValidations target = new ClassWithValidations(null, "someValue", "someValue");
         BeanPropertyBindingResult errors = new BeanPropertyBindingResult(target, "classWithValidations");
-        notNullValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutScope"), "matchingScope", errors);
+        notNullOrEmptyValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutScope"), "matchingScope", errors);
         assertEquals(1, errors.getFieldErrors().size());
         assertEquals("value should not be null", errors.getFieldErrors().get(0).getDefaultMessage());
     }
@@ -39,7 +39,7 @@ public class NotNullValidatorTest {
     public void shouldReportErrorWhenFieldIsNull_AndBothFieldAndValidationScopeIsNotSpecified() throws NoSuchFieldException {
         ClassWithValidations target = new ClassWithValidations(null, null, null);
         BeanPropertyBindingResult errors = new BeanPropertyBindingResult(target, "classWithValidations");
-        notNullValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutScope"), null, errors);
+        notNullOrEmptyValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutScope"), null, errors);
         assertEquals(1, errors.getFieldErrors().size());
     }
 
@@ -47,7 +47,7 @@ public class NotNullValidatorTest {
     public void shouldReportErrorWhenFieldIsNull_AndFieldScopeMatchesValidationScope() throws NoSuchFieldException {
         ClassWithValidations target = new ClassWithValidations("someValue", null, "someValue");
         BeanPropertyBindingResult errors = new BeanPropertyBindingResult(target, "classWithValidations");
-        notNullValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithMatchingScope"), "matchingScope", errors);
+        notNullOrEmptyValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithMatchingScope"), "matchingScope", errors);
         assertEquals(1, errors.getFieldErrors().size());
         assertEquals("value should not be null", errors.getFieldErrors().get(0).getDefaultMessage());
     }
@@ -56,20 +56,20 @@ public class NotNullValidatorTest {
     public void shouldNotReportErrorWhenFieldIsNull_AndFieldScopeDoesNotMatchValidationScope() throws NoSuchFieldException {
         ClassWithValidations target = new ClassWithValidations("someValue", "someValue", null);
         BeanPropertyBindingResult errors = new BeanPropertyBindingResult(target, "classWithValidations");
-        notNullValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutMatchingScope"), "someScope", errors);
+        notNullOrEmptyValidator.validateField(target, ClassWithValidations.class.getDeclaredField("fieldWithoutMatchingScope"), "someScope", errors);
         assertEquals(0, errors.getFieldErrors().size());
     }
 
     @Data
     public static class ClassWithValidations {
 
-        @NotNull
+        @NotNullOrEmpty
         private String fieldWithoutScope;
 
-        @NotNull(scope = "matchingScope")
+        @NotNullOrEmpty(scope = "matchingScope")
         private String fieldWithMatchingScope;
 
-        @NotNull(scope = "nonMatchingScope")
+        @NotNullOrEmpty(scope = "nonMatchingScope")
         private String fieldWithoutMatchingScope;
 
         public ClassWithValidations(String fieldWithoutScope, String fieldWithMatchingScope, String fieldWithoutMatchingScope) {
