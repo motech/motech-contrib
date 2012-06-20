f79e3e1a775c237c301eac72c3b1dd8e45af21ea
Aravind SV <arvind.sv@gmail.com>
2012-06-20 11:02:55 +0530

    Harshit & Duck | Fixing build for quartz upgrade.

--- motech-retry/src/test/java/org/motechproject/retry/RetryServiceIT.java
+++ motech-retry/src/test/java/org/motechproject/retry/RetryServiceIT.java
@@ -9,6 +9,7 @@ import org.motechproject.retry.dao.AllRetries;
 import org.motechproject.retry.domain.Retry;
 import org.motechproject.retry.domain.RetryRequest;
 import org.motechproject.retry.service.RetryServiceImpl;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,13 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.quartz.impl.matchers.GroupMatcher.jobGroupEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext-retry.xml")
@@ -57,11 +62,11 @@ public class RetryServiceIT {
     private void removeQuartzJobs() {
         try {
             Scheduler scheduler = schedulerFactoryBean.getScheduler();
-            String[] groupNames = scheduler.getJobGroupNames();
+            List<String> groupNames = scheduler.getJobGroupNames();
             for (String group : groupNames) {
-                String[] jobNames = scheduler.getJobNames(group);
-                for (String job : jobNames)
-                    scheduler.deleteJob(job, group);
+                Set<JobKey> jobNames = scheduler.getJobKeys(jobGroupEquals(group));
+                for (JobKey jobKey : jobNames)
+                    scheduler.deleteJob(jobKey);
             }
         } catch (SchedulerException e) {
             e.printStackTrace();
