2bdc80b96979c1a8bd163bce6e470a9ed29c0c77
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-08-06 10:20:24 +0200

    llewczynski | #159 | Moved old version of the kookoo module from main repository

---
+++ motech-ivr-kookoo-v1/src/main/java/org/motechproject/ivr/kookoo/domain/KookooIVRMessage.java
@@ -0,0 +1,16 @@
+package org.motechproject.ivr.kookoo.domain;
+
+import org.motechproject.ivr.message.IVRMessage;
+
+public class KookooIVRMessage implements IVRMessage {
+
+    @Override
+    public String getText(String key) {
+        return null;
+    }
+
+    @Override
+    public String getWav(String key, String preferredLangCode) {
+        return null;
+    }
+}
---
+++ motech-ivr-kookoo-v1/src/test/java/org/motechproject/ivr/kookoo/componentsfortest/EventRelayForTest.java
@@ -0,0 +1,10 @@
+package org.motechproject.ivr.kookoo.componentsfortest;
+
+import org.motechproject.scheduler.domain.MotechEvent;
+import org.motechproject.scheduler.event.EventRelay;
+
+public class EventRelayForTest implements EventRelay {
+    @Override
+    public void sendEventMessage(MotechEvent motechEvent) {
+    }
+}
