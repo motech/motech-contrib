a2974e8d312fff9f0c3ae02179a7b39cf9b5e289
f1codz <imdad.ahmed@gmail.com>
2012-10-03 15:00:01 +0530

    Imdad | #258 | Changes so that csvImport can handle multiple file processing

--- motech-bulk-export-import/src/main/java/org/motechproject/importer/CSVDataImporter.java
+++ motech-bulk-export-import/src/main/java/org/motechproject/importer/CSVDataImporter.java
@@ -21,8 +21,13 @@ public class CSVDataImporter {
         dataImporter.importData(args[0],args[1]);
     }
 
-    public void importData(String entity, String file) {
-        URL resource = CSVDataImporter.class.getClassLoader().getResource(file);
-        allCSVDataImportProcessor.get(entity).process(resource == null? file : resource.getPath());
+    public void importData(String entity, String... filePaths) {
+        String[] absoluteFilePaths = new String[filePaths.length];
+        for(int i = 0; i < absoluteFilePaths.length; ++i){
+            URL resource = CSVDataImporter.class.getClassLoader().getResource(filePaths[i]);
+            absoluteFilePaths[i] = resource == null? filePaths[i] : resource.getPath();
+        }
+
+        allCSVDataImportProcessor.get(entity).process(absoluteFilePaths);
     }
 }
