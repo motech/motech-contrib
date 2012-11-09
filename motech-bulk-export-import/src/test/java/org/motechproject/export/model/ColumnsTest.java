868fa80b59c5bbbf858047811d71408c5f1c43da
Vandana <vandanag@vandanag-Latitude-E6510.(none)>
2012-11-09 12:41:13 +0530

    Vishnu,Vandana - #73 - Modified the Columns logic to consider inherited annotated methods as well.

---
+++ motech-bulk-export-import/src/test/java/org/motechproject/export/model/ColumnsTest.java
@@ -0,0 +1,51 @@
+package org.motechproject.export.model;
+
+import org.junit.Test;
+import org.motechproject.export.annotation.ExportValue;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ColumnsTest {
+
+    @Test
+    public void shouldGetInheritedAnnotatedMethods(){
+        Columns columns = new Columns(Dummy.class);
+        assertEquals(3,columns.size());
+        assertEquals("dummyField",columns.get(0).name());
+        assertEquals("parentField1",columns.get(1).name());
+        assertEquals("parentField2",columns.get(2).name());
+    }
+}
+
+
+class Dummy extends DummyParent{
+    private String field;
+
+    private String anotherField;
+
+    @ExportValue(column="dummyField", index = 0)
+    public String getField() {
+        return field;
+    }
+
+    private String getAnotherField() {
+        return anotherField;
+    }
+}
+
+class DummyParent{
+
+    private String parentField1;
+    private String parentField2;
+
+    public void methodFromParent(){}
+
+    @ExportValue(column="parentField1", index = 1)
+    public String getParentField1() {
+        return parentField1;
+    }
+    @ExportValue(column="parentField2", index = 2)
+    public String getParentField2() {
+        return parentField2;
+    }
+}
