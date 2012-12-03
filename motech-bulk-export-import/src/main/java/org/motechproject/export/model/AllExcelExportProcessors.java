87287db1711bc24c7dda23eb06a91ea7ee3e88b0
Vishal Naik <vishalnaik@thoughtworks.com>
2012-12-03 19:00:13 +0530

    Vishal #0 Updating  from Repository to Component. @Repository was failing with NoSuchBeanDefinitionException when used along with JPA repositories.

--- motech-bulk-export-import/src/main/java/org/motechproject/export/model/AllExcelExportProcessors.java
+++ motech-bulk-export-import/src/main/java/org/motechproject/export/model/AllExcelExportProcessors.java
@@ -2,17 +2,20 @@ package org.motechproject.export.model;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.Hashtable;
 import java.util.Map;
 
 import static org.motechproject.export.model.ExcelExportProcessor.isValidDataSource;
 
-@Repository
+@Component
 public class AllExcelExportProcessors implements BeanPostProcessor {
 
     private Map<String, ExcelExportProcessor> reportDataSources = new Hashtable<String, ExcelExportProcessor>();
+
+    public AllExcelExportProcessors() {
+    }
 
     public ExcelExportProcessor get(String groupName) {
         return reportDataSources.get(groupName);
