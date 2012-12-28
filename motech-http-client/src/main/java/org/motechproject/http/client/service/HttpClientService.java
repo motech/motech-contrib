b2bffcb716b5d2701693ed86a59f5434104b39d5
Vishal Naik <vishalnaik@thoughtworks.com>
2012-12-28 16:51:11 +0530

    Vishal, Abhi #0 Added support for Http Headers while using HttpClientService

--- motech-http-client/src/main/java/org/motechproject/http/client/service/HttpClientService.java
+++ motech-http-client/src/main/java/org/motechproject/http/client/service/HttpClientService.java
@@ -1,8 +1,10 @@
 package org.motechproject.http.client.service;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public interface HttpClientService {
     void put(String url, Serializable data);
     void post(String url, Serializable data);
+    void post(String url, Serializable data, HashMap<String, String> headers);
 }
