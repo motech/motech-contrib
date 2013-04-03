36a9bf3c65cd0efad42b72460556eab0a32917cf
Vishal Naik <vishalnaik@thoughtworks.com>
2013-04-03 16:24:18 +0530

    Vishal #0 minor refactor

--- motech-call-log/src/main/java/org/motechproject/calllog/repository/GenericCallLogRepository.java
+++ motech-call-log/src/main/java/org/motechproject/calllog/repository/GenericCallLogRepository.java
@@ -2,7 +2,8 @@ package org.motechproject.calllog.repository;
 
 
 import org.motechproject.calllog.domain.CallLog;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GenericCallLogRepository extends MotechJpaRepository<CallLog> {
+public interface GenericCallLogRepository extends JpaRepository<CallLog, Long> {
 
 }
