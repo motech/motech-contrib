d4cc51fd88d7df0934447dbb56a865d220d05f53
Abhishek <asharma@thoughtworks.com>
2013-04-12 10:50:31 +0530

    Abhi #0 Adding numeric validation to the attempt number

--- motech-call-log/src/main/java/org/motechproject/calllog/request/OutboundDetails.java
+++ motech-call-log/src/main/java/org/motechproject/calllog/request/OutboundDetails.java
@@ -3,6 +3,7 @@ package org.motechproject.calllog.request;
 import lombok.Data;
 import org.motechproject.calllog.validation.DateTimeFormat;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
@@ -24,6 +25,7 @@ public class OutboundDetails implements Serializable {
     private String attemptTime;
 
     @NotNull
+    @Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "Attempt must be a number")
     @Size(min = 1)
     private String attempt;
 }
